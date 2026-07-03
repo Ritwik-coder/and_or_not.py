1231c540b1848b0a99e18add8290851e79f36d2a
Ritwik-coder <ritwik@websoftline.com>
2026-07-02 20:22:27 -0400

    Add files via upload

---
+++ and_or_not.py
@@ -0,0 +1,30 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c :
+    print("all the numbers have a boolien value of true")
+else:
+    print("one of the numbers has a booliein value of false")
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0 :
+    print("One of the values are true")
+else:
+    print("all of them are false")
+
+
+a = 10
+b = -10
+c = 0
+
+if c > 0 or b > 0 :
+    print("One of the values are true")
+else:
+    print("all of them are false")
+
+
+
